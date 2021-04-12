a5dd9885db4f21c9cf92a7bfbbbb037d40b8f1c1
Akhand Patel <akhand3108patel@gmail.com>
2021-04-12 17:12:06 +0530

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Shakespeare Translator
+
+Who wouldn't love to talk like shakespeare. Catch that girl with youur shakespeare english right now.
+
+The website just translates english to shakespearen english.
+Go, take your shot right now.
---
+++ index.js
@@ -0,0 +1,34 @@
+const inputBox = document.querySelector("#text-input")
+const outputBox = document.querySelector("#text-output")
+const translateButton = document.querySelector("#button-translate")
+const clearButton = document.querySelector("#button-clear")
+
+translateButton.addEventListener("click", translateHandler)
+clearButton.addEventListener("click", clearHandler)
+
+function translateHandler(event) {
+  let input = inputBox.value
+  let toFetchUrl =
+    "https://api.funtranslations.com/translate/shakespeare.json?text=" +
+    encodeURI(input)
+
+  console.log(toFetchUrl)
+
+  fetch(toFetchUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data", data)
+      if (data.error) {
+        throw new Error("Please do not type enter key")
+      }
+      outputBox.innerText = data.contents.translated
+    })
+    .catch((error) => {
+      alert(error.message), console.log(error)
+    })
+}
+
+function clearHandler(event) {
+  inputBox.value = ""
+  outputBox.innerText = ""
+}
